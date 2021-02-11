7c6c5f30e1a9af3d556b8330a4f8e8fc43facd1a
Ehmannr <ehmannr@etown.edu>
2021-02-11 11:17:29 -0500

    this is step 4/5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-world
 a hello world program to learn git commands
+### this is a level 3 markdown for step 3
---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello{
+    /**
+     *
+     */
+    private static final String HELLO_TEST = "hello test";
+
+    public static void main(String[] args) {
+    System.out.println(HELLO_TEST);
+    }      
+    
+}
